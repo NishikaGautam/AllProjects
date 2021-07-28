67c2a3fda5591eec38939946fe4c3008ebe222ba
NishikaGautam <88071397+NishikaGautam@users.noreply.github.com>
2021-07-28 10:14:04 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+<div class="wrapper">
+Footer Goes here 
+<p>
+2021 All rights reserved, Restaurant.
+</p>
+</div>
+</div>
+<!-- Footer Section Ends -->
+</body>
+</html>
---
+++ index1.php
@@ -0,0 +1,33 @@
+
+<?php include('partials/menu.php'); ?>
+
+<!-- Main Content Section Starts -->
+<div class="main-content text-center">
+<div class="wrapper">
+<h2> DASHBOARD </h2>
+<div class="col-4 text center">
+<h3>5</h3>
+<br />
+Categories
+</div>
+<div class="col-4 text center">
+<h3>5</h3>
+<br />
+Categories
+</div>
+<div class="col-4 text center">
+<h3>5</h3>
+<br />
+Categories
+</div>
+<div class="col-4 text center">
+<h3>5</h3>
+<br />
+Categories
+</div>
+<div class="clearfix"></div>
+</div>
+</div>
+<!-- Main Content Section Ends -->
+
+<?php include('partials/footer.php'); ?>
